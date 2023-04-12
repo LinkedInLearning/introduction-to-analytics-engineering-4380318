868f48aecaf679501eaf9abe060563e2d1d3f182
Amata <47616644+arl480@users.noreply.github.com>
2023-04-11 23:18:40 -0400

    Update make-pr-here.sql

--- second-challenge/make-pr-here.sql
+++ second-challenge/make-pr-here.sql
@@ -1,13 +1,11 @@
 create view product_perf as
     select
-        p.ProductId as Product_ID
+        p.Product_Id as Product_ID
         , p.ProductName as Product_Name
         , p.Category as Product_Category
-        , p.Description as Product_Description
         , p.Price as Product_Price
-        , p.ImageURL as Product_ImageURL
         , sum(o.Quantity) as Total_Product_Sold
-        , count(o.OrderNum) as Total_Product_Orders
+        , count(o.Order_Id) as Total_Product_Orders
 
 from products p
 join orders o
