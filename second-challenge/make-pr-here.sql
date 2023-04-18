5b309a04899b09a43e52573e018a31d66d0b0ebf
Amata <47616644+arl480@users.noreply.github.com>
2023-04-18 14:06:21 -0700

    Update make-pr-here.sql

--- second-challenge/make-pr-here.sql
+++ second-challenge/make-pr-here.sql
@@ -6,7 +6,10 @@ create view product_perf as
         , p.Price as Product_Price
         , sum(o.Quantity) as Total_Product_Sold
         , count(o.Order_Id) as Total_Product_Orders
+        , count(distinct c.state) as Total_States
 
 from products p
 join orders o
     on o.product_id = p.product_id
+join customers c
+    on c.cust_id = o.cust_id
